3749017f3c8f1e7cd280e99042b29ec6d454cf5f
Popovych Mariia <mariia.popovych@powercoders.org>
2024-10-31 13:06:58 +0100

    authors done

--- author/scripts.js
+++ author/scripts.js
@@ -48,3 +48,30 @@ let authors = [
         lastName: 'Libsig'
     }
 ];
+
+const authorPhotos = document.querySelectorAll('main ul li');
+
+authorPhotos.forEach((li) => {
+
+    const img = li.querySelector('img');
+    if (img) {
+        
+        const imgName = img.src.split('/').pop();
+        
+        const imgAuthorName = imgName.replace("tile_", "").replace(".jpg", "");
+
+        const author = authors.find(author =>
+            `${author.firstName.toLowerCase()}_${author.lastName.toLowerCase()}` === imgAuthorName
+        );
+
+        if (author) {
+            const authorName = document.createElement('h2');
+            authorName.textContent = `${author.firstName} ${author.lastName}`;
+            
+            li.appendChild(authorName);
+            
+            img.alt = `${author.firstName} ${author.lastName}`;
+            img.title = `${author.firstName} ${author.lastName}`;
+        }
+    }
+});
